81948b97eaf732b6cc29d0e6729db8e48a43367e
Stephan <stephan.alsleben@hotmail.de>
2016-05-22 14:26:27 +0200

    folder added

--- sources/test.py
+++ sources/test.py
@@ -1,7 +1,9 @@
 #!/bin/python3.5
 
-import variable
+import sys
+sys.path.append('../class')
 import Class
+
 
 stone = Class.stone("pedone", "file.grafic", "witeh", "front")
 print (stone.name, stone.grafic, stone.team, stone.direction)
