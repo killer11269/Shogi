4ec9d6e1dbfe682ec6b87000608e4f3c0f371371
Stephan <stephan.alsleben@hotmail.de>
2016-06-09 20:53:20 +0200

    Word Correction

--- sources/test.py
+++ sources/test.py
@@ -5,5 +5,5 @@ sys.path.append('../class')
 import Class
 
 
-stone = Class.stone("pedone", 0xFF, "file.grafic", "witeh", "front")
+stone = Class.stone("pedone", 0xFF, "file.grafic", "withe", "front")
 print (stone.name, stone.move, stone.grafic, stone.team, stone.direction)
